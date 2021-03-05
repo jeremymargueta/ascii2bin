666791e098408c4c333291852b58ff40fae62192
jeremymargueta <45952986+jeremymargueta@users.noreply.github.com>
2021-03-04 23:39:29 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -17,26 +17,17 @@ int main (int argc, char * argv[], char ** envp)
    
     retval = read(0, &ascii_value, 1);
 
-    // if( ascii_value != 0 || ascii_value != 1 )
-    // {
-    //     fprintf(stderr, "Error Detected!\n"); 
-    //     return 1;
-    // }
-
     while (retval == 1 && ascii_value != '\n')
     {
+        if( ascii_value != 0 && ascii_value != 1 )
+        {
+            fprintf(stderr, "Error Detected!\n"); 
+            return 1;
+        }
+
         digit = ascii_value - offset;
-        // if( ascii_value != 0 || ascii_value != 1 )
-        // {
-        //     fprintf(stderr, "Error Detected!\n"); 
-        //     return 1;
-        // }
         number = (number << 1) + digit;
-          if( ascii_value != 0 || ascii_value != 1 )
-    {
-        fprintf(stderr, "Error Detected!\n"); 
-        return 1;
-    } 
+  
         retval = read(0, &ascii_value, 1);
     }
     printf("%u\n", number);
